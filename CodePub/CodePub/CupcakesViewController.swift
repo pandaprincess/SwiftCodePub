f07d138bfb7c24f8822391c9fb52f91506bf6803
Susanne Moseby <susannemoseby@gmail.com>
2015-01-21 23:52:31 +0100

    setting up a segue and adding a new controller

---
+++ CodePub/CodePub/CupcakesViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CupcakesViewController.swift
+//  CodePub
+//
+//  Created by Susanne Moseby on 21/01/15.
+//  Copyright (c) 2015 codepub. All rights reserved.
+//
+
+import UIKit
+
+class CupcakesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.purpleColor()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
